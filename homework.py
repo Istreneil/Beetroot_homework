a6d322f5401296e24d9017a541a24b77920523a1
Iryna Olasyuk <90067559+Istreneil@users.noreply.github.com>
2021-09-24 20:26:31 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,99 @@
+import json
+import csv
+
+student_fields = ['first_name', 'last_name', 'email', 'age', 'address', 'gender']
+
+STUDENTS = []
+
+TEST_STUDENTS = [
+    ['Mary', 'D', 'mail@mail.com', '19', 'Huston', 'F'],
+    ['John', 'S', 'new_mail@mail.com', '21', 'London', 'M'],
+    ['Andy', 'H', 'more_mail@mail.com', 'sexteen', 'Brighton', 'M']
+]
+
+
+def add_student():
+    student = {}
+    for field in student_fields:
+        field = field.capitalize().replace('_', ' ')
+        student[field] = input('Enter {}\t'.format(field))
+        if field == 'age':
+            try:
+                int(student['age'])
+            except:
+                student['age'] = input('Enter age as number\t')
+    STUDENTS.append(student)
+
+
+def calculate_avg_age():
+    try:
+        total_age = 0
+        for student in STUDENTS:
+            total_age += int(student['age'])
+        avgerage_age = total_age / len(STUDENTS)
+        print('Average age is {}'.format(avgerage_age))
+    except ValueError:
+        print('Cannot calculate average age')
+    except Exception as e:
+        print(str(e))
+
+
+def print_student(student):
+    for field in student:
+        print(field, '\t', student[field])
+
+
+def print_students_list():
+    for student in STUDENTS:
+        print_student(student)
+        print('\t')
+
+
+def load_students():
+    for test_student in TEST_STUDENTS:
+        STUDENTS.append(dict(zip(student_fields, test_student)))
+
+
+def dump_students():
+    with open('student_data.json', 'w') as file:
+        json.dump(STUDENTS, file)
+
+
+def dump_csv():
+    with open('student_data.csv', 'w') as file:
+        writer = csv.DictWriter(file, fieldnames=student_fields)
+        writer.writeheader()
+        for student in STUDENTS:
+            writer.writerow(student)
+
+
+def load_csv(file_path='student_data.csv'):
+    with open('student_data.csv', 'w') as file:
+        reader = csv.reader(file)
+        for student in STUDENTS:
+            print(dump_csv())
+
+
+def load_from_json(file_path='student_data.json'):
+    with open(file_path, 'r') as read_file:
+        STUDENTS.extend(json.load(read_file))
+
+
+ACTIONS = {
+    'add': add_student,
+    'avg_age': calculate_avg_age,
+    'load': load_students,
+    'print': print_students_list,
+    'dump': dump_students,
+    'dump_csv': dump_csv,
+    'load_json': load_from_json,
+    'load_csv': load_csv
+}
+
+if __name__ == '__main__':
+    while True:
+        action = input('Desired action:\t')
+        if action in ACTIONS:
+            ACTIONS.get(action)()
+        else:
+            break
